013150c9716ce53ce441308f9edfa31ec9809692
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-09-08 22:10:22 +0800

    Register router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+// import router
+import router from "./router";
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    // register router
+    router,
+    render: h => h(App),
+}).$mount("#app");
